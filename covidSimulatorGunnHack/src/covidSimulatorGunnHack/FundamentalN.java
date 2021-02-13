17a5209ddf358ad09f64a8dad835187c8206dcca
Anika Sood <soodanika@gmail.com>
2021-02-13 12:40:11 -0800

    fundamental n updates

--- covidSimulatorGunnHack/src/covidSimulatorGunnHack/FundamentalN.java
+++ covidSimulatorGunnHack/src/covidSimulatorGunnHack/FundamentalN.java
@@ -2,10 +2,10 @@ package covidSimulatorGunnHack;
 
 public class FundamentalN {
 
-	double[][] qRegion, rRegion;
-	double[][] f; //inverse
-	double[][] fr;
-	double[][] res;
+	double[][] qRegion, rRegion; //regions of transition matrix
+	double[][] f; //[identity - q]^(-1)
+	double[][] fr; //f times r
+	double[][] res; //probability of recovering/dying after being infected
 	
 	public FundamentalN()
 	{
@@ -28,12 +28,15 @@ public class FundamentalN {
 		}
 	}
 	
+	//f = [identity - q]^(-1)
 	public void inverse() {
-		//identity minus 
+		//identity minus qRegion 
 		f[0][0] = 1 - qRegion[0][0];
 		f[0][1] = 0 - qRegion[0][1];
 		f[1][0] = 0 - qRegion[1][0];
 		f[1][1] = 1 - qRegion[1][1];
+		
+		//inverse of result
 		double det = f[0][0] * f[1][1] - f[0][1] * f[1][0];
 		f[0][0] = f[1][1] / det;
 		f[0][1] = - f[0][1] / det;
@@ -42,15 +45,17 @@ public class FundamentalN {
 		
 	}
 
+	//get final results (f times r)
 	public double[][] possibility() {
 		for(int i = 0; i < 2; i++) {
 			for(int j = 0; j < 2; j++) {
 				fr[i][j] = f[i][0] * rRegion[0][j] + f[i][1] * rRegion[1][j];
 			}
 		}
-
-	res[0][0] = fr[1][0];
-	res[0][1] = fr[1][1];
+	
+	//probability of recovering/dying after being infected
+	res[0][0] = fr[1][0]; //recovering
+	res[0][1] = fr[1][1]; //dying
 	
 	return res;
 	}
